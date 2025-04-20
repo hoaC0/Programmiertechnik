ebf979901426e86e068c06112f8b53fb092885cd
Hoa Chau <hoachau1010@gmail.com>
2025-04-20 22:28:58 +0200

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// Funktionen
+int a(int a,int b, int c, int d, int e, int f, int g, int h, int i, int j){
+    return a + b + c + d + e + f + g + h + i + j;
+}
+
+void b(int a,int b, int c, int d, int e, int f, int g, int h, int i, int j){
+    std::cout << a + b + c + d + e + f + g + h + i + j;
+}
+
+
+int main(){
+    a(1,2,3,4,5,6,7,8,9,10);
+    return 0;
+}
