c735f6a5d896ade64a34e383c9ce08f0170d0e98
Hoa Chau <hoachau1010@gmail.com>
2025-04-21 01:12:43 +0200

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// Grundlagen: C++ 
+
+// Aufgabe 1: Korrekter Programmaufbau mit allen notwendigen Bestandteilen
+
+// Aufgabe 2: Verwendung unterschiedlicher Datentypen (int, double, float, char, bool)
+
+// Aufgabe 3: Korrekte Deklaration und Initialisierung von Variablen
+
+/*  Aufgabe 4: Verwendung von Kommentaren (einzeilig und mehrzeilig) */
+
+// Aufgabe 5: Ausgabe der Speichergrößen der verwendeten Datentypen mit sizeof
+
+int A = 10;
+double B = 6.9;
+float C = 20.222;
+char D = 'F';
+bool E = false;
+
+
+int main() {
+    std::cout << A << std::endl;
+    std::cout << B << std::endl;
+    std::cout << C << std::endl;
+    std::cout << D << std::endl;
+    std::cout << E << std::endl;
+    return 0;
+}
